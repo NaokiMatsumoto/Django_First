d88bb04b052f1c1c0f3f8ca9b54f06d6be9acc72
NaokiMatsumoto <9nmatsumoto@gmail.com>
2019-05-11 13:12:58 +0900

    modify

---
+++ my_video_renatal/videos/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from . import models
+
+
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year', 'title', 'length']
+    search_fields = ['title']
+    list_filter = ['release_year', 'length', 'title']
+    list_display = ['release_year', 'length', 'title']
+    list_editable = ['length']
+
+
+admin.site.register(models.Customer)
+admin.site.register(models.Movie, MovieAdmin)
